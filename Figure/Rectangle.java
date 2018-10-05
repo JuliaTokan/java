52c4b59ae2abed107c455936600ddf087069e406
JuliaTokan <43521750+JuliaTokan@users.noreply.github.com>
2018-10-05 19:10:20 +0300

    Add files via upload

---
+++ Figure/Rectangle.java
@@ -0,0 +1,17 @@
+package Figure;
+
+public class Rectangle extends Figure {
+    private double firstSide;
+    private double secondSide;
+
+    public Rectangle(double firstSide, double secondSide) {
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+    }
+
+    @Override
+    public double square() {
+        double square = firstSide * secondSide;
+        return square;
+    }
+}
