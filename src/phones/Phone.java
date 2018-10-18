844cd98b52d08958a1adfcf5705194e2c7a2b807
JuliaTokan <43521750+JuliaTokan@users.noreply.github.com>
2018-10-18 22:43:12 +0300

    Add files via upload

---
+++ src/phones/Phone.java
@@ -0,0 +1,42 @@
+package phones;
+
+public abstract class Phone {
+    protected boolean touch;
+    protected boolean hasWifi;
+    protected int screenSize;
+    protected int numCalls;
+    protected int numSMS;
+
+    public Phone() {
+        System.out.println("Phone constructor");
+        numCalls = 0;
+        numSMS = 0;
+    }
+
+    public boolean isTouch() {
+        return touch;
+    }
+
+    public boolean isHasWifi() {
+        return hasWifi;
+    }
+
+    public int getScreenSize() {
+        return screenSize;
+    }
+
+    public int getNumCalls() {
+        return numCalls;
+    }
+
+    public int getNumSMS() {
+        return numSMS;
+    }
+
+    public void call(String number) {
+        System.out.println("Phone class is calling " + number);
+        numCalls++;
+    }
+
+    public abstract void sendSMS(String number, String message);
+}
